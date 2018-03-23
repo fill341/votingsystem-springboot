0f74266bd97b18654b80c156f48f179991394a77
Alexey Shulayakov <shaakem@gmail.com>
2018-03-24 01:06:53 +0700

    added Menu and Dish entities

---
+++ src/main/java/com/shaakem/votingsystem/model/Dish.java
@@ -0,0 +1,74 @@
+package com.shaakem.votingsystem.model;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "dishes")
+public class Dish extends AbstractNamedEntity {
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull
+    private Restaurant restaurant;
+
+    @ManyToMany(fetch = FetchType.EAGER,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            }, mappedBy = "dishes")
+    private Set<Menu> menus = new HashSet<>();
+
+    public Dish() {
+    }
+
+    public Dish(String name, int price) {
+        this(null, name, price);
+    }
+
+    public Dish(Integer id, String name, int price) {
+        super(id, name);
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public Set<Menu> getMenus() {
+        return menus;
+    }
+
+    public void setMenus(Set<Menu> menus) {
+        this.menus = menus;
+    }
+
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "id=" + id +
+                ", name=" + name +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ src/main/java/com/shaakem/votingsystem/model/Menu.java
@@ -0,0 +1,76 @@
+package com.shaakem.votingsystem.model;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "menus", uniqueConstraints = {@UniqueConstraint(columnNames = {"restaurant_id", "date_time"}, name = "menus_unique_restaurant_datetime_idx")})
+public class Menu extends AbstractBaseEntity {
+    @Column(name = "date_time", nullable = false)
+    @NotNull
+    private LocalDateTime dateTime;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull
+    private Restaurant restaurant;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "menu_dish",
+            joinColumns = {@JoinColumn(name = "menu_id")},
+            inverseJoinColumns = {@JoinColumn(name = "dish_id")}
+    )
+    private Set<Dish> dishes = new HashSet<>();
+
+    public Menu() {
+    }
+
+    public Menu(LocalDateTime dateTime) {
+        this(null, dateTime);
+    }
+
+    public Menu(Integer id, LocalDateTime dateTime) {
+        super(id);
+        this.dateTime = dateTime;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public Set<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(Set<Dish> dishes) {
+        this.dishes = dishes;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "id=" + id +
+                ", dateTime=" + dateTime +
+                '}';
+    }
+}
