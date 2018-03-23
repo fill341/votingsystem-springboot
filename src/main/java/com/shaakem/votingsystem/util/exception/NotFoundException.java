b8728b9ab91a2e05afdc0d7b249305d5330f2d04
Alexey Shulayakov <shaakem@gmail.com>
2018-03-24 00:40:59 +0700

    added ValidationUtil and NotFoundException classes

---
+++ src/main/java/com/shaakem/votingsystem/util/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.shaakem.votingsystem.util.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
