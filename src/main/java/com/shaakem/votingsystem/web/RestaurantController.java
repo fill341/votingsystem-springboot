ee92075232e0ef8c8a120624772040982ccbd670
Alexey Shulayakov <shaakem@gmail.com>
2018-03-24 00:41:30 +0700

    added Restaurant controller

---
+++ src/main/java/com/shaakem/votingsystem/web/RestaurantController.java
@@ -0,0 +1,36 @@
+package com.shaakem.votingsystem.web;
+
+import com.shaakem.votingsystem.model.Restaurant;
+import com.shaakem.votingsystem.repository.restaurant.RestaurantRepository;
+import com.shaakem.votingsystem.to.RestaurantTo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(RestaurantController.REST_URL)
+public class RestaurantController {
+    static final String REST_URL = "/admin/rest/restaurants";
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<RestaurantTo> getAll() {
+        log.info("Get all restaurants.");
+
+        List<RestaurantTo> list = new ArrayList<>();
+        for (Restaurant restaurant : restaurantRepository.getAll()) {
+            RestaurantTo restaurantTo = new RestaurantTo(restaurant);
+            list.add(restaurantTo);
+        }
+
+        return list;
+    }
+}
