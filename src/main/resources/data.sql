2ca5d8f8ba14f158b4c6ea991cea45a8461acd3c
shaakem <shaakem@gmail.com>
2018-04-09 23:00:05 +0700

    refactored data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,11 +7,6 @@ DELETE FROM votes;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
--- INSERT INTO users (name, email, password) VALUES
---   ('Admin', 'admin@gmail.com', '{noop}admin'),
---   ('User01', 'user01@gmail.com', '{noop}user01'),
---   ('User02', 'user02@gmail.com', '{noop}user02');
-
 INSERT INTO users (name, email, password) VALUES
   ('Admin', 'admin@gmail.com', 'admin'),
   ('User01', 'user01@gmail.com', 'user01'),
@@ -21,11 +16,6 @@ INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100001),
   ('ROLE_ADMIN', 100000),
   ('ROLE_USER', 100000);
-
-INSERT INTO votes (user_id, date_time) VALUES
-  (100001, '2018-01-19 10:00:00'),
-  (100001, '2018-01-20 10:00:00'),
-  (100002, '2018-01-19 08:30:00');
 
 INSERT INTO restaurants (name, address) VALUES
   ('McDonalds', 'Lenina 1'),
@@ -47,3 +37,8 @@ INSERT INTO menu_dish (menu_id, dish_id) VALUES
   (100009, 100012),
   (100009, 100013),
   (100010, 100014);
+
+INSERT INTO votes (user_id, date_time) VALUES
+  (100001, '2018-01-19 10:00:00'),
+  (100001, '2018-01-20 10:00:00'),
+  (100002, '2018-01-19 08:30:00');
