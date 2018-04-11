2e71af61186a097deb3bc4ad0570e87e8e701334
shaakem <shaakem@gmail.com>
2018-04-12 01:52:28 +0700

    added userService into SecurityConfig

--- src/main/java/com/shaakem/votingsystem/config/SecurityConfig.java
+++ src/main/java/com/shaakem/votingsystem/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.shaakem.votingsystem.config;
 
 import com.shaakem.votingsystem.repository.user.UserRepository;
+import com.shaakem.votingsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -19,6 +21,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    @Qualifier("userService")
+    private UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -41,6 +47,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider);
+        auth.authenticationProvider(authProvider).userDetailsService(userService);
     }
 }
