c988fb29b115a7e370b024dad506294274fa51bf
shaakem <shaakem@gmail.com>
2018-04-12 01:53:08 +0700

    added AuthorizedUser

---
+++ src/main/java/com/shaakem/votingsystem/AuthorizedUser.java
@@ -0,0 +1,50 @@
+package com.shaakem.votingsystem;
+
+import com.shaakem.votingsystem.model.User;
+import com.shaakem.votingsystem.to.UserTo;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import static java.util.Objects.requireNonNull;
+
+public class AuthorizedUser extends org.springframework.security.core.userdetails.User {
+    private static final long serialVersionUID = 1L;
+
+    private UserTo userTo;
+
+    public AuthorizedUser(User user) {
+        super(user.getEmail(), user.getPassword(), user.isEnabled(), true, true, true, user.getRoles());
+        this.userTo = new UserTo(user.getId(), user.getName(), user.getEmail(), user.getPassword());
+    }
+
+    public static AuthorizedUser safeGet() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) {
+            return null;
+        }
+        Object principal = auth.getPrincipal();
+        return (principal instanceof AuthorizedUser) ? (AuthorizedUser) principal : null;
+    }
+
+    public static AuthorizedUser get() {
+        AuthorizedUser user = safeGet();
+        requireNonNull(user, "No authorized user found");
+        return user;
+    }
+
+    public int getId() {
+        return userTo.getId();
+    }
+
+    public static int id() {
+        return get().userTo.getId();
+    }
+
+    public void update(UserTo newTo) {
+        userTo = newTo;
+    }
+
+    public UserTo getUserTo() {
+        return userTo;
+    }
+}
