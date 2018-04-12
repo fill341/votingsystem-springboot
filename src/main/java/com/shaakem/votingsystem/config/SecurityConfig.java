8d852924dd22f5a50a7e13fd147ab7e3a451adb2
shaakem <shaakem@gmail.com>
2018-04-13 01:06:23 +0700

    replaced UserRepository to UserService in SecurityConfig

--- src/main/java/com/shaakem/votingsystem/config/SecurityConfig.java
+++ src/main/java/com/shaakem/votingsystem/config/SecurityConfig.java
@@ -1,11 +1,8 @@
 package com.shaakem.votingsystem.config;
 
-import com.shaakem.votingsystem.repository.user.UserRepository;
 import com.shaakem.votingsystem.service.UserService;
-import com.shaakem.votingsystem.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -22,17 +19,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private CustomAuthenticationProvider authProvider;
 
     @Autowired
-    private UserRepository userRepository;
-
-//    @Autowired
-//    @Qualifier("userService")
-//    private UserService userService;
-
-    @Bean
-    public UserService userService() {
-        return new UserServiceImpl(userRepository) {
-        };
-    }
+    @Qualifier("userService")
+    private UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -49,13 +37,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                                 authenticationManager()),
                         UsernamePasswordAuthenticationFilter.class
                 )
-                .addFilterBefore(new JwtAuthenticationFilter(userRepository),
+                .addFilterBefore(new JwtAuthenticationFilter(userService),
                         UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider).userDetailsService(userService());
+        auth.authenticationProvider(authProvider).userDetailsService(userService);
     }
 
 
