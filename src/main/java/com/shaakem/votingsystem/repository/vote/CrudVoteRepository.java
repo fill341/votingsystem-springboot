27ca0ee3ff79a73b25df43813edb093f3f64644b
shaakem <shaakem@gmail.com>
2018-04-15 14:06:31 +0700

    replaced LocalDateTime to LocalDate in vote repository layer

--- src/main/java/com/shaakem/votingsystem/repository/vote/CrudVoteRepository.java
+++ src/main/java/com/shaakem/votingsystem/repository/vote/CrudVoteRepository.java
@@ -6,14 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 @Transactional(readOnly = true)
 public interface CrudVoteRepository extends JpaRepository<Vote, Integer> {
-    @Query("SELECT v FROM Vote v WHERE v.user.id=:userId ORDER BY v.dateTime DESC")
+    @Query("SELECT v FROM Vote v WHERE v.user.id=:userId ORDER BY v.localDate DESC")
     List<Vote> getAll(@Param("userId") int userId);
 
-    @Query("FROM Vote v WHERE v.dateTime=:today")
-    List<Vote> getAllPerToday(@Param("today") LocalDateTime today);
+    @Query("FROM Vote v WHERE v.localDate=:today")
+    List<Vote> getAllPerToday(@Param("today") LocalDate today);
 }
--- src/main/java/com/shaakem/votingsystem/repository/vote/VoteRepositoryImpl.java
+++ src/main/java/com/shaakem/votingsystem/repository/vote/VoteRepositoryImpl.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -20,7 +19,7 @@ public class VoteRepositoryImpl implements VoteRepository {
 
     @Override
     public List<Vote> getAllPerToday() {
-        LocalDateTime today = LocalDateTime.now();
+        LocalDate today = LocalDate.now();
         return crudVoteRepository.getAllPerToday(today);
     }
 }
