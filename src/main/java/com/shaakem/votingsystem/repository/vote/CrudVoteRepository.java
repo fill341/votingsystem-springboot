ee719267ef3f479d7e7a2c430f9c25bc59b1f18b
shaakem <shaakem@gmail.com>
2018-04-26 02:14:42 +0700

    added save method into CrudVoteRepository

--- src/main/java/com/shaakem/votingsystem/repository/vote/CrudVoteRepository.java
+++ src/main/java/com/shaakem/votingsystem/repository/vote/CrudVoteRepository.java
@@ -6,14 +6,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Transactional(readOnly = true)
 public interface CrudVoteRepository extends JpaRepository<Vote, Integer> {
-    @Query("SELECT v FROM Vote v WHERE v.user.id=:userId ORDER BY v.localDate DESC")
+    @Override
+    @Transactional
+    Vote save(Vote vote);
+
+    @Query("SELECT v FROM Vote v WHERE v.user.id=:userId ORDER BY v.dateTime DESC")
     List<Vote> getAll(@Param("userId") int userId);
 
-    @Query("FROM Vote v WHERE v.localDate=:today")
-    List<Vote> getAllPerToday(@Param("today") LocalDate today);
+    @Query("FROM Vote v WHERE v.dateTime=:today")
+    List<Vote> getAllPerToday(@Param("today") LocalDateTime today);
 }
